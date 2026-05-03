ab2e4c6a2aaa3e808b2dfc362adcc181d076e448
鈴木文登 <suzukifumito@fumi-macbook-air.local>
2026-05-03 15:38:37 +0900

    fix: tap

--- Assets/Script/TapGame.cs
+++ Assets/Script/TapGame.cs
@@ -6,34 +6,65 @@ public class TapGame : MonoBehaviour
     [SerializeField]
     private InputAction _pointerClickAction;
     [SerializeField]
+    private InputAction _pointerPressAction;
+    [SerializeField]
     private Camera _camera;
     [SerializeField]
     private Transform _tapEffect;
+    [SerializeField]
+    private Transform _pressEffect;
 
-    void OnStarted(InputAction.CallbackContext context)
+    void OnTap(InputAction.CallbackContext context)
     {
+        if(Pointer.current == null) return;
+
         var tapEffectInstance = Instantiate(_tapEffect, transform);
-        if(context.control.device is Pointer pointer)
-        {
-            var screenPos = pointer.position.ReadValue();
-            var worldPos = _camera.ScreenToWorldPoint(screenPos);
-            tapEffectInstance.position = new Vector3(worldPos.x, worldPos.y, 0);
-        }
+
+        var screenPos = Pointer.current.position.ReadValue();
+        var worldPos = _camera.ScreenToWorldPoint(screenPos);
+        tapEffectInstance.position = new Vector3(worldPos.x, worldPos.y, 0);
+
+        Debug.Log(screenPos);
+    }
+
+    void OnPressPerformed(InputAction.CallbackContext context)
+    {
+        if(Pointer.current == null) return;
+
+        _pressEffect.gameObject.SetActive(true);
+    }
+
+    void OnPressCanceled(InputAction.CallbackContext context)
+    {
+        _pressEffect.gameObject.SetActive(false);
     }
 
     void OnEnable()
     {
-        if(_pointerClickAction == null) return;
-
         _pointerClickAction.Enable();
-        _pointerClickAction.started += OnStarted;
+        _pointerClickAction.started += OnTap;
+
+        _pointerPressAction.Enable();
+        _pointerPressAction.performed += OnPressPerformed;
+        _pointerPressAction.canceled += OnPressCanceled;
     }
     
     void OnDisable()
     {
-        if(_pointerClickAction == null) return;
-
         _pointerClickAction.Disable();
-        _pointerClickAction.started -= OnStarted;
+        _pointerClickAction.started -= OnTap;
+
+        _pointerPressAction.Enable();
+        _pointerPressAction.performed += OnPressPerformed;
+        _pointerPressAction.canceled += OnPressCanceled;
+    }
+
+    void Update()
+    {
+        if(Pointer.current == null || _pressEffect.gameObject.activeInHierarchy is false) return;
+
+        var screenPos = Pointer.current.position.ReadValue();
+        var worldPos = _camera.ScreenToWorldPoint(screenPos);
+        _pressEffect.position = new Vector3(worldPos.x, worldPos.y, 0);
     }
 }
