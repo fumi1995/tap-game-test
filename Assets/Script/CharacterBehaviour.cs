c027c62e3e4ae62ff97c50678911dd2e8d768991
鈴木文登 <suzukifumito@fumi-macbook-air.local>
2026-05-03 17:08:19 +0900

    add: character sassy

---
+++ Assets/Script/CharacterBehaviour.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    [SerializeField, Range(0f, 30f)]
+    private float _lifeTime = 10f;
+
+    private Animator _animator;
+    private float _lifeTimeTimer;
+    private bool _isDied;
+
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _lifeTimeTimer = _lifeTime;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isDied) return;
+
+        _lifeTimeTimer -= Time.deltaTime;
+
+        if(_lifeTimeTimer <= 0)
+        {
+            StartCoroutine("DestoryAsync");
+            _isDied = true;
+        }
+    }
+
+    IEnumerator DestoryAsync()
+    {
+        _animator.Play("out");
+        Debug.Log($"{Time.frameCount} out");
+
+        yield return null;
+
+        var layerNum = 0;
+		var currentAnimatorState = _animator.GetCurrentAnimatorStateInfo(layerNum);
+
+		while(currentAnimatorState.normalizedTime < 1)
+        {
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Script/CharacterManager.cs
+++ Assets/Script/CharacterManager.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private CharacterBehaviour[] _characters;
+    [SerializeField, Range(0f, 30f)]
+    private float _spawnInterval = 10f;
+    [SerializeField, Range(0f, 30f)]
+    private float _spawnIntervalRandamize = 5f;
 
-    // Update is called once per frame
+    private float _spawnTimer;
+
     void Update()
     {
-        
+        _spawnTimer -= Time.deltaTime;
+
+        if(_spawnTimer <= 0)
+        {
+            var choicedCharacterIdx = Random.Range(0, _characters.Length - 1);
+            var choicedCharacter = _characters[choicedCharacterIdx];
+
+            float randomX = Random.Range(0f, 1f);
+            float randomY = Random.Range(0f, 1f);
+
+            float zDistance = 10f;
+
+            Vector3 screenPos = new Vector3(randomX, randomY, zDistance);
+
+            Vector3 worldPos = Camera.main.ViewportToWorldPoint(screenPos);
+
+            Instantiate(choicedCharacter, worldPos, Quaternion.identity);
+            
+            _spawnTimer = _spawnInterval + Random.Range(0f, _spawnIntervalRandamize);
+        }
     }
 }
